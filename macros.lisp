ca4a997cafbad75424bcb0f77bc12b34d31f7add
Ben Diedrich <bldiedrich@gmail.com>
2010-09-15 16:29:00 -0400

    got rid of the use of a separate MA class to simplify code, despite Fateman's warning to the contrary. It serves my purposes.

--- macros.lisp
+++ macros.lisp
@@ -2,16 +2,6 @@
 
 (in-package :bld-gen)
 
-(defclass ma ()
-  ((e :initarg :e :reader e :initform 0)))
-
-(defmethod print-object ((m ma) stream)
-  (format stream "~a" (e m)))
-
-(defgeneric ma (form))
-(defmethod ma (form)
-  (make-instance 'ma :e form))
-
 (defun to-cl (n)
   (find-symbol (symbol-name n) :cl-user))
 
@@ -31,18 +21,14 @@
        (defgeneric ,two-fun (arg1 arg2))
        (defmethod ,two-fun ((arg1 number) (arg2 number))
 	 (,cl-op arg1 arg2))
-       (defmethod ,two-fun ((arg1 number) (arg2 ma))
-	 (,two-fun (ma arg1) arg2))
-       (defmethod ,two-fun ((arg1 ma) (arg2 number))
-	 (,two-fun arg1 (ma arg2)))
-       (defmethod ,two-fun ((arg1 ma) (arg2 ma))
-	 (make-instance (type-of arg1) :e (simp (list ',op (e arg1) (e arg2)))))
+       (defmethod ,two-fun (arg1 arg2)
+	 (simp (list ',op arg1 arg2)))
        ,@(when min-one-arg?
 	       (list `(defgeneric ,one-fun (arg))
 		     `(defmethod ,one-fun ((arg number))
 			(,cl-op arg))
-		     `(defmethod ,one-fun ((arg ma))
-			(make-instance (type-of arg) :e (simp (list ',op (e arg)))))))
+		     `(defmethod ,one-fun (arg)
+			(simp (list ',op arg)))))
        (defun ,op (,@(when min-one-arg? '(arg1)) &rest args)
 	 (if args (reduce (function ,two-fun) 
 			  ,(if min-one-arg? 
@@ -58,12 +44,8 @@
      (defgeneric ,op (arg1 arg2))
      (defmethod ,op ((arg1 number) (arg2 number))
        (,(to-cl op) arg1 arg2))
-     (defmethod ,op ((arg1 number) (arg2 ma))
-       (,op (ma arg1) arg2))
-     (defmethod ,op ((arg1 ma) (arg2 number))
-       (,op arg1 (ma arg2)))
-     (defmethod ,op ((arg1 ma) (arg2 ma))
-       (make-instance (type-of arg1) :e (simp (list ',op (e arg1) (e arg2)))))))
+     (defmethod ,op (arg1 arg2)
+       (simp (list ',op arg1 arg2)))))
 
 (defmacro defarith1 (op)
   "Define generic arithmetic function of 1 argument"
@@ -71,8 +53,8 @@
      (defgeneric ,op (arg))
      (defmethod ,op ((arg number))
        (,(to-cl op) arg))
-     (defmethod ,op ((arg ma))
-       (make-instance (type-of arg) :e (simp (list ',op (e arg)))))))
+     (defmethod ,op (arg)
+       (simp (list ',op arg)))))
 
 (defmacro defarith2opt (op)
   "Define generic arithmetic function with 1 required argument and 2nd optional argument"
@@ -83,17 +65,13 @@
        (defgeneric ,two-fun (arg1 arg2))
        (defmethod ,two-fun ((arg1 number) (arg2 number))
 	 (,cl-op arg1 arg2))
-       (defmethod ,two-fun ((arg1 number) (arg2 ma))
-	 (,two-fun (ma arg1) arg2))
-       (defmethod ,two-fun ((arg1 ma) (arg2 number))
-	 (,two-fun arg1 (ma arg2)))
-       (defmethod ,two-fun ((arg1 ma) (arg2 ma))
-	 (make-instance (type-of arg1) :e (simp (list ',op (e arg1) (e arg2)))))
+       (defmethod ,two-fun (arg1 arg2)
+	 (simp (list ',op arg1 arg2)))
        (defgeneric ,one-fun (arg))
        (defmethod ,one-fun ((arg number))
 	 (,cl-op arg))
-       (defmethod ,one-fun ((arg ma))
-	 (make-instance (type-of arg) :e (simp (list ',op (e arg)))))
+       (defmethod ,one-fun (arg)
+	 (simp (list ',op arg)))
        (defun ,op (arg1 &optional arg2)
 	 (if arg2 (,two-fun arg1 arg2)
 	     (,one-fun arg1))))))
