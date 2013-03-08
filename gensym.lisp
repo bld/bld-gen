177af353ff265770f2cdca0f12965bccfd944daa
Ben Diedrich <bldiedrich@gmail.com>
2013-03-07 16:33:15 -0800

    Changed to use new embeddable-maxima version of bld-maxima

--- gensym.lisp
+++ gensym.lisp
@@ -1,3 +1,8 @@
 (in-package :bld-gensym)
 
+(defun simp (expr)
+  "Simplify lisp arithmetic expression using updated table with symbols in BLD-GENSYM"
+  (let ((bld-maxima::*maxima-lisp-table-intern* *maxima-lisp-table-gensym*))
+    (bld-maxima:simp expr)))
+
 (defsymops)
--- sympackage.lisp
+++ sympackage.lisp
@@ -3,7 +3,6 @@
 
 (defpackage :bld-gensym
   (:use cl bld-gen)
-  (:import-from bld-maxima simp)
   (:import-from bld-gen defmeth1 defmeth2 defmeth12 *ops*)
   (:shadowing-import-from bld-gen
 			  + - * / expt
@@ -13,3 +12,9 @@
 			  asinh acosh atanh
 			  log exp sqrt abs
 			  min max signum))
+
+(in-package :bld-gensym)
+
+(defparameter *maxima-lisp-table-gensym*
+  (loop for (m l) in bld-maxima::*maxima-lisp-table-intern*
+     collect (list m (intern (format nil "~a" l) :bld-gensym))))
