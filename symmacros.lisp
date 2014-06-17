a80e6590468b58cbb8ae2fbcfdcf9c2204784d30
Ben Diedrich <bldiedrich@gmail.com>
2014-06-16 18:37:33 -0700

    Updated to work with SOCKET branch of BLD-MAXIMA

--- symmacros.lisp
+++ symmacros.lisp
@@ -1,20 +1,29 @@
 (in-package :bld-gensym)
 
 (defmacro defsymmeth (op nargs &optional min1arg)
+  "Define symbolic method given operator, number of args, and -
+optionally - specify with T or NIL whether the method has a minimum of
+1 arg. E.g. for (- a)."
   (cond
+    ;; Don't define symbolic method for division: already handled by bld-gen
     ((equal op '/) nil)
-    ((equal nargs 1) `(defmethod ,op ((arg t)) (simp-socket (list ',op arg))))
+    ;; Single argument
+    ((equal nargs 1) `(defmethod ,op ((arg t)) (simp (list ',op arg))))
+    ;; 1, 2, or more arguments
     ((and (or (equal nargs 2) (equal nargs 'bld-gen::n)) min1arg)
      `(defmeth12 ,op ((arg1 t) (arg2 t))
-	((simp-socket (list ',op arg1)))
-	((simp-socket (list ',op arg1 arg2)))))
+	((simp (list ',op arg1)))
+	((simp (list ',op arg1 arg2)))))
+    ;; 2 arguments
     ((and (equal nargs 2) (null min1arg))
-     `(defmethod ,op ((arg1 t) (arg2 t)) (simp-socket (list ',op arg1 arg2))))
+     `(defmethod ,op ((arg1 t) (arg2 t)) (simp (list ',op arg1 arg2))))
+    ;; 2 or more arguments
     ((and (equal nargs 'bld-gen::n) (null min1arg))
-     `(defmeth2 ,op ((arg1 t) (arg2 t)) (simp-socket (list ',op arg1 arg2))))
+     `(defmeth2 ,op ((arg1 t) (arg2 t)) (simp (list ',op arg1 arg2))))
     (t (error "Invalid arguments."))))
 
 (defmacro defsymops ()
+  "Define all symbolic operations from *ops* table"
   `(progn
      ,@(loop for op in *ops*
 	  collect `(defsymmeth ,@op))))
--- sympackage.lisp
+++ sympackage.lisp
@@ -3,7 +3,14 @@
 
 (defpackage :bld-gensym
   (:use cl bld-gen)
-  (:import-from bld-maxima simp-socket with-maxima delay)
+  (:import-from bld-maxima
+		simp
+		with-maxima 
+		delay 
+		trigreduce 
+		trigexpand 
+		trigsimp 
+		trigrat)
   (:import-from bld-gen defmeth1 defmeth2 defmeth12 *ops*)
   (:shadowing-import-from bld-gen
 			  + - * / expt
@@ -13,4 +20,4 @@
 			  asinh acosh atanh
 			  log exp sqrt abs
 			  min max signum)
-  (:export with-maxima delay))
+  (:export with-maxima delay simp trigreduce trigexpand trigsimp trigrat))
